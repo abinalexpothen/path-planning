833e8f000829fb11a89b8def5bab28d835b26750
abinalexpothen <abinalexpothen@gmail.com>
2021-10-23 20:55:37 -0400

    add planner class

--- src/model.cpp
+++ src/model.cpp
@@ -12,9 +12,25 @@ class Map
         {0, 0, 0, 1, 1, 0}
     };
 
-    int mapWidth = grid.size();
-    int mapHeight = grid[0].size();
+    int mapWidth = grid[0].size();
+    int mapHeight = grid.size();
+};
 
+
+class Planner
+{
+  public:
+    std::vector<int> start{0, 0};
+    std::vector<int> goal{4, 5};
+    int cost = 1;
+    std::vector<std::vector<int> > movement{
+        {-1, 0},
+        {0, -1},
+        {1, 0},
+        {0, 1}
+    };
+    
+    std::vector<char> movement_arrows{'^', '<', 'v', '>'};
 };
 
 int main()
